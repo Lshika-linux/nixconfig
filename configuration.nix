889e2c7f60ab17ef574a8e552b3cfe7afeaf0435
Lshika <tssblackfox@gmail.com>
2026-06-14 13:12:13 +0200

    init: migrate to flakes

---
+++ configuration.nix
@@ -0,0 +1,161 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.plymouth.enable = true;
+  # A pro to, aby se heslo k disku zadávalo v GUI:
+  boot.initrd.systemd.enable = true;
+
+  networking.hostName = "node_1"; # Define your hostname.
+  # Pick only one of the below networking options.
+  networking.wireless.iwd.enable = true;  # Enables wireless support via IWD.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  time.timeZone = "Europe/Prague";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd river";
+      };
+    };
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  # hardware.pulseaudio.enable = true;
+  # OR
+  services.pipewire = {
+     enable = true;
+     pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.rafi = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" "video" "audio" "input" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+     ];
+  };
+
+  # programs.firefox.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     wget
+     micro
+     kitty
+     git
+     curl
+     river
+     wofi
+     mako
+     wl-clipboard
+     udiskie
+     kanshi
+     grim
+     slurp
+     swaylock-effects
+     swayidle
+     brightnessctl
+     wlr-randr
+     firefox
+     lxqt.lxqt-policykit
+     impala
+     bluetuith
+     htop
+  ];
+
+  fonts.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "Hack" ]; })
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  
+  security.polkit.enable = true;
+  
+  services.udisks2.enable = true;
+
+  zramSwap.enable = true;
+
+  services.tlp = {
+    enable = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 40;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+      START_CHARGE_THRESH_BAT1 = 40;
+      STOP_CHARGE_THRESH_BAT1 = 80;
+    };
+  };
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nixpkgs.config.allowUnfree = true;
+  
+  programs.river.enable = true;
+  programs.waybar.enable = true;
+  programs.dconf.enable = true;
+
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
+
---
+++ home.nix
@@ -0,0 +1,60 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "rafi";
+  home.homeDirectory = "/home/rafi";
+  home.stateVersion = "24.11";
+
+  # 1. Tady instalujeme balíčky
+  home.packages = with pkgs; [
+    swaybg
+    wofi
+    wlogout
+    fastfetch
+    kitty
+  ];
+
+  # 2. Tady konfigurujeme to, co chceme
+  # Službu swaybg.enable a image smaž, ty v HM pro swaybg nefungují takto přímo
+  
+  programs.home-manager.enable = true;
+
+  programs.kitty = {
+      enable = true;
+      settings = {
+        background_opacity = "0.85";
+        font_size = "12.0";
+        confirm_os_window_close = 0;
+  
+        # Tvá paleta z exit.sh
+        foreground = "#cdd6f4";
+        background = "#1a1e2e";
+        cursor     = "#c8848b";
+  
+        # ANSI barvy (přesně podle barev v tvém swaylock skriptu)
+        color0  = "#2e3a59"; # Ring color
+        color1  = "#e06060"; # Ring/BS highlight
+        color2  = "#5b8dd9"; # Ring ver color
+        color3  = "#c8848b"; # Key HL color
+        color4  = "#5b8dd9"; # Blue accent
+        color5  = "#c8848b"; # Magenta/Accent
+        color6  = "#88c0d0"; # Soft cyan
+        color7  = "#cdd6f4"; # Text color
+      };
+  };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      nrs = "sudo nixos-rebuild switch";
+      ll = "ls -alh";
+      hms = "home-manager switch";
+    };
+  };
+
+  xdg.configFile."fastfetch/config.jsonc".text = ''
+    {
+      "modules": ["os", "host", "kernel", "uptime", "shell", "terminal", "cpu", "memory"]
+    }
+  '';
+}
