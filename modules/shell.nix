8a3152190ddf899427721df9d4ef93822987bafa
Lshika <tssblackfox@gmail.com>
2026-07-20 22:33:31 +0200

    shell: add captive-on/off aliases for portal login

--- modules/shell.nix
+++ modules/shell.nix
@@ -7,6 +7,8 @@
       ll = "ls -alh";
       hms = "home-manager switch";
       bt = "bluetuith";
+      captive-on  = "sudo resolvectl dnsovertls wlan0 no && sudo resolvectl domain wlan0 '' && sudo resolvectl dns wlan0 '' && echo 'DNS unlocked - log in, then run captive-off'";
+      captive-off = "sudo systemctl restart systemd-resolved && echo 'DNS locked back down'";
     };
   };
 
